399823dec6c890a80f613c7ead6f124a8f217909
Sammit <sammitkhade77@gmail.com>
2022-04-17 13:04:36 +0530

    added some math recursion

--- src/Math/Product.java
+++ src/Math/Product.java
@@ -3,10 +3,14 @@ public class Product
 {
     public static void main(String[] args)
     {
-
+        System.out.println(product(123));
     }
     public static int product(int no)
     {
-        
+        if(no%10==no)
+        {
+            return no;
+        }
+        return (no%10) * product(no/10);
     }
 }
