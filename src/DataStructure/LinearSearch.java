789a08101986b6300a982e15bd178f4ed80758bd
Sammit <sammitkhade77@gmail.com>
2022-04-17 15:12:48 +0530

    added LinearSearch.java

---
+++ src/DataStructure/LinearSearch.java
@@ -0,0 +1,18 @@
+package DataStructure;
+import java.util.Arrays;
+public class LinearSearch
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {23,34,1,45,63,53};
+        System.out.println(linearSearch(arr,0,45));
+    }
+    public static boolean linearSearch(int[] arr,int start,int target)
+    {
+        if(start == arr.length)
+        {
+            return false;
+        }
+        return arr[start] == target || linearSearch(arr,++start,target);
+    }
+}
